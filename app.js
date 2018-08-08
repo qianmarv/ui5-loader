308cbfdbc32903986c35cba295ca340f083c3d78
qianmarv <qianmarv@gmail.com>
2018-08-08 15:57:31 +0800

    remove reload

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ const serve = require('koa-static');
 const Koa = require('koa');
 const app = new Koa();
 
-const reload = require('reload')
-
 // app.use(serve('webapps/fin.cons.fsimap.maintain'));
 app.use(serve('webapps/Walkthrough/webapp'))
 app.use(serve('webapps/sapui5-dist'))
 
 // app.use(serve({rootDir: 'webapps', rootPath: 'fin.cons.fsimap.maintain'}))
-reload(app);
 
 app.listen(3000,function(){
     console.log('listening on port 3000');
-    if(process.send){
-        process.send('online');
-    };
 });
